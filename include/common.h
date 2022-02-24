e42f8c7b7831c600866cfa0a46b1fd0b34c5231f
Daniil Ostashev <dostashev@snapchat.com>
2022-02-24 17:36:50 +0000

    Add rng

--- include/common.h
+++ include/common.h
@@ -13,12 +13,23 @@
 #include <map>
 #include <cstdlib>
 #include <unordered_map>
+#include <random>
 
 using namespace std;
+
+#ifndef SEED
+#define SEED 1337
+#endif
 
 struct Context {
     using TSolution = int;
     TSolution Solution;
+
+    std::mt19937_64 rng;
+
+    Context() {
+        rng.seed(SEED);
+    }
 
     void Input() {
         
